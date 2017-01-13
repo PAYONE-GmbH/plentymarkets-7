af2527cae9204e13f826fa0bf83b80028a4c75a5
Simon Heinen <heinen@arvatis.com>
2017-01-13 16:46:46 +0100

    add listener for GetPaymentMethodContent

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -2,81 +2,13 @@
 
 namespace Payone\Services;
 
-use Plenty\Modules\Payment\Contracts\PaymentRepositoryContract;
-use Plenty\Modules\Payment\Method\Contracts\PaymentMethodRepositoryContract;
 use Plenty\Modules\Basket\Models\Basket;
-use Plenty\Modules\Plugin\Libs\Contracts\LibraryCallContract;
-use Plenty\Plugin\ConfigRepository;
-use Plenty\Modules\Account\Address\Contracts\AddressRepositoryContract;
 
-
+/**
+ * Class PaymentService
+ */
 class PaymentService
 {
-
-    /**
-     * @var PaymentMethodRepositoryContract
-     */
-    private $paymentMethodRepository;
-
-    /**
-     * @var PaymentRepositoryContract
-     */
-    private $paymentRepository;
-
-    /**
-     * @var PaymentHelper
-     */
-    private $paymentHelper;
-
-    /**
-     * @var LibraryCallContract
-     */
-    private $libCall;
-
-    /**
-     * @var AddressRepositoryContract
-     */
-    private $addressRepo;
-
-    /**
-     * @var ConfigRepository
-     */
-    private $config;
-
-    /**
-     * @var SessionStorageService
-     */
-    private $sessionStorage;
-
-
-    /**
-     * PaymentService constructor.
-     *
-     * @param PaymentMethodRepositoryContract $paymentMethodRepository
-     * @param PaymentRepositoryContract $paymentRepository
-     * @param ConfigRepository $config
-     * @param PaymentHelper $paymentHelper
-     * @param LibraryCallContract $libCall
-     * @param AddressRepositoryContract $addressRepo
-     * @param SessionStorageService $sessionStorage
-     */
-    public function __construct(
-        PaymentMethodRepositoryContract $paymentMethodRepository,
-        PaymentRepositoryContract $paymentRepository,
-        ConfigRepository $config,
-        PaymentHelper $paymentHelper,
-        LibraryCallContract $libCall,
-        AddressRepositoryContract $addressRepo,
-        SessionStorageService $sessionStorage
-    ) {
-        $this->paymentMethodRepository = $paymentMethodRepository;
-        $this->paymentRepository = $paymentRepository;
-        $this->paymentHelper = $paymentHelper;
-        $this->libCall = $libCall;
-        $this->addressRepo = $addressRepo;
-        $this->config = $config;
-        $this->sessionStorage = $sessionStorage;
-    }
 
     /**
      * Get the type of payment from the content of the PayPal container

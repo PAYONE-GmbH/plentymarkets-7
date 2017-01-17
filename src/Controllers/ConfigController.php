b4da94fa92e4e4ad890c2cfc5832ff718d3cb050
Simon Heinen <heinen@arvatis.com>
2017-01-17 09:46:54 +0100

    remove logger calls

--- src/Controllers/ConfigController.php
+++ src/Controllers/ConfigController.php
@@ -2,8 +2,6 @@
 
 namespace Payone\Controllers;
 
-use Payone\Services\MailLogger;
-use Plenty\Plugin\ConfigRepository;
 use Plenty\Plugin\Controller;
 
 /**
@@ -11,32 +9,13 @@ use Plenty\Plugin\Controller;
  */
 class ConfigController extends Controller
 {
-
-    /**
-     * @var ConfigRepository
-     */
-    private $configRepo;
-
-    /**
-     * @param ConfigRepository $configRepo
-     */
-    public function __construct(ConfigRepository $configRepo)
-    {
-        $this->configRepo = $configRepo;
-    }
-
     /**
      * @return void
      */
     public function index()
     {
         echo 'index';
-        $config = '';
 
-        foreach ($this->configRepo->get('Payone') as $key => $value) {
-            $config .= $key . '=>' . $value . PHP_EOL;
-        }
-        echo $config;
     }
 
     /**
@@ -44,7 +23,6 @@ class ConfigController extends Controller
      */
     public function test()
     {
-        MailLogger::log(__METHOD__ . ': calling test action');
         echo 'test';
 
     }

e53db74671aad11422ad14572e59c698a668d094
Simon Heinen <heinen@arvatis.com>
2017-04-10 11:40:08 +0200

    handle api errors more strict

--- resources/lib/doAuth.php
+++ resources/lib/doAuth.php
@@ -6,31 +6,32 @@ use Payone\Request\RequestFactory;
 use Payone\Request\Types;
 use Payone\Response\ClientErrorResponse;
 
-$basket = SdkRestApi::getParam('basket');
-$basketItems = SdkRestApi::getParam('basketItems');
-$country = SdkRestApi::getParam('country');
-$shippingAddress = SdkRestApi::getParam('shippingAddress');
-$shippingAddress['country'] = $country['isoCode2'];
-$context = SdkRestApi::getParam('context');
-$order = SdkRestApi::getParam('order');
-$customer = SdkRestApi::getParam('customer');
-$shippingProvider = SdkRestApi::getParam('shippingProvider');
-
-$data['basket'] = $basket;
-$data['basketItems'] = $basketItems;
-$data['shippingAddress'] = $shippingAddress;
-$data['context'] = $context;
-$data['order'] = $order;
-$data['customer'] = $customer;
-$data['shippingProvider'] = $shippingProvider;
-
-$paymentMethod = SdkRestApi::getParam('paymentMethod');
-$orderId = SdkRestApi::getParam('orderId');
-
-$request = RequestFactory::create(Types::AUTHORIZATION, $paymentMethod, $orderId, $data);
-$client = new PostApi(new Client());
-
 try {
+    $basket = SdkRestApi::getParam('basket');
+    $basketItems = SdkRestApi::getParam('basketItems');
+    $country = SdkRestApi::getParam('country');
+    $shippingAddress = SdkRestApi::getParam('shippingAddress');
+    $shippingAddress['country'] = $country['isoCode2'];
+    $context = SdkRestApi::getParam('context');
+    $order = SdkRestApi::getParam('order');
+    $customer = SdkRestApi::getParam('customer');
+    $shippingProvider = SdkRestApi::getParam('shippingProvider');
+
+    $data['basket'] = $basket;
+    $data['basketItems'] = $basketItems;
+    $data['shippingAddress'] = $shippingAddress;
+    $data['context'] = $context;
+    $data['order'] = $order;
+    $data['customer'] = $customer;
+    $data['shippingProvider'] = $shippingProvider;
+
+    $paymentMethod = SdkRestApi::getParam('paymentMethod');
+    $orderId = SdkRestApi::getParam('orderId');
+
+    $request = RequestFactory::create(Types::AUTHORIZATION, $paymentMethod, $orderId, $data);
+    $client = new PostApi(new Client());
+
+
     $response = $client->doRequest($request->toArray());
 } catch (Exception $e) {
     $errorResponse = new ClientErrorResponse($e->getMessage());

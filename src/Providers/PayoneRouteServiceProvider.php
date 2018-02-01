7e28c1223da8c641726eb0872d53db62fa2441d4
Simon Heinen <heinen@arvatis.com>
2018-02-01 16:07:21 +0100

    #4048 register controller action for sepa mandate

--- src/Providers/PayoneRouteServiceProvider.php
+++ src/Providers/PayoneRouteServiceProvider.php
@@ -30,6 +30,6 @@ class PayoneRouteServiceProvider extends RouteServiceProvider
         $router->post('payone/checkout/storeCCCheckResponse', 'Payone\Controllers\CheckoutController@storeCCCheckResponse');
         $router->post('payone/checkout/storeAccountData', 'Payone\Controllers\CheckoutController@storeAccountData');
         $router->get('payone/error', 'Payone\Controllers\CheckoutController@redirectWithNotice');
-        $router->get('payone/getSepaMandateStep', 'Payone\Controllers\CheckoutController@getSepaMandateStep');
+        $router->get('payone/checkout/getSepaMandateStep', 'Payone\Controllers\CheckoutController@getSepaMandateStep');
     }
 }

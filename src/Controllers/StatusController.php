03103b3fc550c2caf7f7fa7b7a73448cf3a3a381
Simon Heinen <heinen@arvatis.com>
2018-02-26 14:03:30 +0100

    #4481 nicht implementierte Funktionen aus Konfiguration entfernen

--- src/Controllers/StatusController.php
+++ src/Controllers/StatusController.php
@@ -70,10 +70,10 @@ class StatusController extends Controller
             $txaction = $txaction . '_' . $transactionStatus;
         }
 
-        $this->logger->setIdentifier(__METHOD__)
-            ->setReferenceType(Logger::PAYONE_REQUEST_REFERENCE)
-            ->setReferenceValue($txid)
-            ->debug('Controller.Status', $this->request->all());
+        $this->logger->setIdentifier(__METHOD__);
+        $this->logger->setReferenceType(Logger::PAYONE_REQUEST_REFERENCE);
+        $this->logger->setReferenceValue($txid);
+        $this->logger->debug('Controller.Status', $this->request->all());
 
         if ($this->request->get('key') != md5($this->config->get('key'))) {
             return;

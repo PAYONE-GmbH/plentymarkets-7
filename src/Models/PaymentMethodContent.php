627ecb1a533344752907219d53f0c9b3e5b539d4
Simon Heinen <heinen@arvatis.com>
2018-04-04 13:54:47 +0200

    #4844 enable paydirekt payment

--- src/Models/PaymentMethodContent.php
+++ src/Models/PaymentMethodContent.php
@@ -4,7 +4,9 @@ namespace Payone\Models;
 
 use Payone\Methods\PayoneCCPaymentMethod;
 use Payone\Methods\PayoneDirectDebitPaymentMethod;
+use Payone\Methods\PayonePaydirektPaymentMethod;
 use Payone\Methods\PayonePayPalPaymentMethod;
+use Payone\Methods\PayoneSofortPaymentMethod;
 use Plenty\Modules\Payment\Events\Checkout\GetPaymentMethodContent;
 
 /**
@@ -25,6 +27,8 @@ class PaymentMethodContent
             case PayoneCCPaymentMethod::PAYMENT_CODE:
                 return GetPaymentMethodContent::RETURN_TYPE_HTML;
             case PayonePayPalPaymentMethod::PAYMENT_CODE:
+            case PayonePaydirektPaymentMethod::PAYMENT_CODE:
+            case PayoneSofortPaymentMethod::PAYMENT_CODE:
                 return GetPaymentMethodContent::RETURN_TYPE_REDIRECT_URL;
         }
 

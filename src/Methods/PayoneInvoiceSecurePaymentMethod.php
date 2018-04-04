4c43340a7d161113797ebbec5cf731637019f21d
Simon Heinen <heinen@arvatis.com>
2018-04-04 16:29:18 +0200

    #4855 add invoice secure payment

---
+++ src/Methods/PayoneInvoiceSecurePaymentMethod.php
@@ -0,0 +1,13 @@
+<?php
+
+// strict
+
+namespace Payone\Methods;
+
+/**
+ * Class PayoneInvoiceSecurePaymentMethod
+ */
+class PayoneInvoiceSecurePaymentMethod extends PaymentAbstract
+{
+    const PAYMENT_CODE = 'PAYONE_PAYONE_INVOICE_SECURE';
+}

d1897d89279ff8b4861c094a6b252bd24dcf83fb
Ilyes Tascou <ilyes.tascou@plentymarkets.com>
2020-09-09 15:44:18 +0200

    renaming of routes

--- src/Providers/PayoneRouteServiceProvider.php
+++ src/Providers/PayoneRouteServiceProvider.php
@@ -22,11 +22,11 @@ class PayoneRouteServiceProvider extends RouteServiceProvider
         $router->post('payment/payone/checkout/doAuth', 'Payone\Controllers\CheckoutController@doAuth');
         $router->post('payment/payone/checkout/storeCCCheckResponse', 'Payone\Controllers\CheckoutController@storeCCCheckResponse');
         $router->post('payment/payone/checkout/storeAccountData', 'Payone\Controllers\CheckoutController@storeAccountData');
-        $router->get('payment/payone/checkout/amazonPayWidget', 'Payone\Controllers\AmazonPayController@getAmazonPayLoginWidget');
 
         $router->post('payment/payone/checkout/amazonPay/renderWidgets', 'Payone\Controllers\AmazonPayController@renderWidgets');
         $router->post('payment/payone/checkout/amazonPay/setOrderReference', 'Payone\Controllers\AmazonPayController@setOrderReference');
         $router->get('payment/payone/checkout/amazonPay/getOrderReference', 'Payone\Controllers\AmazonPayController@getOrderReference');
+        $router->get('payment/payone/checkout/amazonPay/loginButton', 'Payone\Controllers\AmazonPayController@getAmazonPayLoginWidget');
 
 
         $router->get('payment/payone/error', 'Payone\Controllers\CheckoutController@redirectWithNotice');

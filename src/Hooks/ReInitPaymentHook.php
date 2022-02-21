377f3d2298b9d8a1eb809d1952ed7035d03b8c79
Cosmin Manciu <cosmin.manciu@plentymarkets.com>
2022-02-21 14:35:13 +0200

    Added Amazon Login Button to Confirmation

--- src/Hooks/ReInitPaymentHook.php
+++ src/Hooks/ReInitPaymentHook.php
@@ -5,17 +5,39 @@ namespace Payone\Hooks;
 use Ceres\Helper\LayoutContainer;
 use IO\Extensions\Constants\ShopUrls;
 use IO\Helper\RouteConfig;
+use Payone\Helpers\PaymentHelper;
+use Payone\Services\SettingsService;
+use Plenty\Modules\Order\Contracts\OrderRepositoryContract;
 use Plenty\Modules\Payment\Contracts\PaymentRepositoryContract;
 use Plenty\Modules\Payment\Method\Contracts\PaymentMethodRepositoryContract;
 use Plenty\Modules\Payment\Method\Models\PaymentMethod;
 use Plenty\Plugin\Templates\Twig;
 use Payone\PluginConstants;
-
+use Plenty\Modules\Authorization\Services\AuthHelper;
+use Payone\Methods\PayoneAmazonPayPaymentMethod;
 
 class ReInitPaymentHook
 {
     public function handle(LayoutContainer $layoutContainer, $order)
     {
+
+        /** @var OrderRepositoryContract $orderContract */
+        $orderContract = pluginApp(OrderRepositoryContract::class);
+
+        /** @var \Plenty\Modules\Authorization\Services\AuthHelper $authHelper */
+        $authHelper = pluginApp(AuthHelper::class);
+        //
+        $orderId = $order['id'];
+        $orderNew = $authHelper->processUnguarded(
+            function () use ($orderContract, $orderId) {
+                //unguarded
+                return $orderContract->findOrderById($orderId);
+            }
+        );
+
+
+        $mopId = $orderNew->methodOfPaymentId;
+
         /** @var PaymentMethodRepositoryContract $paymentMethodRepository */
         $paymentMethodRepository = pluginApp(PaymentMethodRepositoryContract::class);
         $paymentMethods          = $paymentMethodRepository->allForPlugin(PluginConstants::NAME);
@@ -25,6 +47,10 @@ class ReInitPaymentHook
                 $paymentIds[] = $paymentMethod->id;
             }
         }
+        /** @var PaymentHelper $paymentHelper */
+        $paymentHelper = pluginApp(PaymentHelper::class);
+        $amazonPayMopId = $paymentHelper->getMopId(PayoneAmazonPayPaymentMethod::PAYMENT_CODE);
+
 
         /** @var ShopUrls $shopUrls */
         $shopUrls = pluginApp(ShopUrls::class);
@@ -39,15 +65,32 @@ class ReInitPaymentHook
         }
 
 
-        /** @var Twig $twig */
-        $twig = pluginApp(Twig::class);
-        $layoutContainer->addContent($twig->render(PluginConstants::NAME . '::Checkout.ReinitPayment', [
-            'order' => $order,
-            "paymentIds" => $paymentIds,
-            'isMyAccount' => $isMyAccount,
-            'isOrderConfirmation' => $isOrderConfirmation,
-            'orderHasPayment' => $orderHasPaymentAssigned
-         ])
-        );
+        /** @var SettingsService $settingsService */
+        $settingsService = pluginApp(SettingsService::class);
+
+        if($mopId == $amazonPayMopId) {
+            /** @var Twig $twig */
+            $twig = pluginApp(Twig::class);
+
+            $layoutContainer->addContent($twig->render(PluginConstants::NAME . '::Checkout.AmazonPayConfirmation', [
+                'selectedPaymentId' => $mopId,
+                'amazonPayMopId' => $amazonPayMopId,
+                'sandbox' => (bool)$settingsService->getPaymentSettingsValue('Sandbox', PayoneAmazonPayPaymentMethod::PAYMENT_CODE)
+            ])
+            );
+        } else {
+
+            /** @var Twig $twig */
+            $twig = pluginApp(Twig::class);
+            $layoutContainer->addContent($twig->render(PluginConstants::NAME . '::Checkout.ReinitPayment', [
+                'order' => $order,
+                "paymentIds" => $paymentIds,
+                'isMyAccount' => $isMyAccount,
+                'isOrderConfirmation' => $isOrderConfirmation,
+                'orderHasPayment' => $orderHasPaymentAssigned,
+
+            ])
+            );
+        }
     }
 }
